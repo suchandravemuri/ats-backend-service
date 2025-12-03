bb1824f091fbc147f91e2da60f3134257e908cc1
suchandravemuri <146853599+suchandravemuri@users.noreply.github.com>
2025-12-03 23:26:05 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.get("/", (_, res) => {
 });
 const connectDB = async () => {
     try {
-        console.log(process.env.MONGO_URI)
+ 
       const conn = await mongoose.connect(process.env.MONGO_URI!);
       console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
